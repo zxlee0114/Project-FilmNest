9c4459832dcd871e484637cb78a4bcd33766363e
s851218 <sin0981802858@gmail.com>
2024-09-18 21:42:31 +0800

    import Swiper 樣式與 JS

---
+++ assets/js/swiper.js
@@ -0,0 +1,16 @@
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
+
+// test
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+// import styles bundle 匯入全模組樣式
+import "swiper/css/bundle";
+
+// import Swiper JS
+import "./assets/js/swiper.js";
 
 console.log("Hello world!");
