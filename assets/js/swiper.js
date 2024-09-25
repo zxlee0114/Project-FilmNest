718558fb72c4d72e701a7afe70dbe3fa08e8d2fd
s851218 <sin0981802858@gmail.com>
2024-09-25 08:43:40 +0800

    新增劇照swiper

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -2,15 +2,38 @@
 import Swiper from "swiper/bundle";
 
 // test
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  effect: "fade",
+// var swiper = new Swiper(".mySwiper", {
+//   spaceBetween: 30,
+//   effect: "fade",
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+// project-intro
+// 劇照 swiper
+// 下方小圖 Swiper
+const projectIntroSwiper = new Swiper(".projectIntroSwiper", {
+  spaceBetween: 24,
+  slidesPerView: 3,
+  navigation: {
+    nextEl: ".projectIntroSwiper-button-next",
+    prevEl: ".projectIntroSwiper-button-prev",
+  },
+});
+// 上方所展示的大張圖片的swiper
+const projectIntroSwiper2 = new Swiper(".projectIntroSwiper2", {
+  spaceBetween: 10,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+  thumbs: {
+    swiper: projectIntroSwiper,
   },
 });
