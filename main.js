07a4fac489668acee0c54364edd52eb2a5fbf3f1
jhdpc <as313234@gmail.com>
2024-10-10 14:07:24 +0800

    留言板、資訊揭露、提案者介紹

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
-
+import "https://unpkg.com/@phosphor-icons/web@2.1.1";
 // import styles bundle 匯入全模組樣式
 import "swiper/css/bundle";
 
